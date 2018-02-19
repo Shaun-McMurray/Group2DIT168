d67c9f13629f39e08af8847c42d11f3666169e1b
Shaun-McMurray <gusmcmsh@student.gu.se>
2018-02-19 11:14:43 +0100

    new build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shaun/Documents/projectexample
-# Build directory: /home/shaun/Documents/projectexample/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(helloworld-Runner "/home/shaun/Documents/projectexample/build/helloworld-Runner")
