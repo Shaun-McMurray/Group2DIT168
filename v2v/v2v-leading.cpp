8674f64872f2d8d2fde8506d516397efa8961873
Tim Jonasson <gusjonati@student.gu.se>
2018-04-11 12:08:21 +0200

    managed to add the v2v-leading.cpp to the cmakelists.txt so it compiles with the project now

--- v2v/v2v-leading.cpp
+++ v2v/v2v-leading.cpp
@@ -1,5 +1,8 @@
-#include "../V2VService.hpp"
+#include "V2VService.hpp"
 
 int main(){
     std::cout << "hello" << std::endl;
+    V2VService* v2vService = new V2VService();
+    std::cout << "bye" << std::endl;
+
 }
