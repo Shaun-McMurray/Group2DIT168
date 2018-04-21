7ca41820ea6ab058681e7b76f585afc24817ef6f
ninauljanic <nina.uljanic@gmail.com>
2018-04-21 21:36:10 +0200

    Attempt at pushing sensor files #2

---
+++ opendlv-device-adc-bbblue/src/opendlv-device-adc-bbblue.cpp
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2018 Ola Benderius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Code is following the example of the MIT-licenced Robotics Cape Library: https://github.com/StrawsonDesign/Robotics_Cape_Installer
+ */ 
+
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <cmath>
+
+#include "cluon-complete.hpp"
+#include "opendlv-standard-message-set.hpp"
+
+int32_t main(int32_t argc, char **argv) {
+  int32_t retCode{0};
+  auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
+  if (0 == commandlineArguments.count("cid") || 0 == commandlineArguments.count("freq") || 0 == commandlineArguments.count("channel")) {
+    std::cerr << argv[0] << " interfaces to the analog-to-digital converters on the BeagleBone Blue." << std::endl;
+    std::cerr << "Usage:   " << argv[0] << " --freq=<frequency> --cid=<OpenDaVINCI session> --channel=<the ADC channel to read> [--id=<Identifier in case of multiple sensors] [--verbose]" << std::endl;
+    std::cerr << "Example: " << argv[0] << " --freq=10 --cid=111 --channel=0" << std::endl;
+    retCode = 1;
+  } else {
+    uint32_t const ID{(commandlineArguments["id"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
+    bool const VERBOSE{commandlineArguments.count("verbose") != 0};
+    uint16_t const CID = std::stoi(commandlineArguments["cid"]);
+    uint32_t const CHANNEL = std::stoi(commandlineArguments["channel"]);
+    float const FREQ = std::stof(commandlineArguments["freq"]);
+
+    int32_t fd = open("/dev/mem", O_RDWR);
+    if (fd == -1) {
+      std::cerr << "Unable to open /dev/mem" << std::endl;
+      return -1;
+    }
+
+    uint32_t const MMAP_OFFSET{0x44C00000};
+    uint32_t const MMAP_SIZE{0x481AEFFF - MMAP_OFFSET};
+
+    uint32_t volatile *map = static_cast<uint32_t *>(mmap(nullptr, MMAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, MMAP_OFFSET));
+    if(map == MAP_FAILED) {
+      close(fd);
+      std::cerr << "Unable to map /dev/mem" << std::endl;
+      return -1;
+    }
+
+    uint32_t const CM_WKUP{0x44E00400};
+    uint32_t const CM_WKUP_ADC_TSC_CLKCTRL{0xBC};
+    uint32_t const MODULEMODE_ENABLE{0x2};
+    
+    map[(CM_WKUP + CM_WKUP_ADC_TSC_CLKCTRL - MMAP_OFFSET) / 4] |= MODULEMODE_ENABLE;
+
+    while (!(map[(CM_WKUP + CM_WKUP_ADC_TSC_CLKCTRL - MMAP_OFFSET) / 4] & MODULEMODE_ENABLE)) {
+      std::this_thread::sleep_for(std::chrono::duration<double>(1e-5));
+      if (VERBOSE) {
+        std::cout << "Waiting for CM_WKUP_ADC_TSC_CLKCTRL to enable with MODULEMODE_ENABLE" << std::endl;
+      }
+    }
+
+    uint32_t const ADC_TSC{0x44E0D000};
+    uint32_t const ADC_CTRL{ADC_TSC + 0x40};
+    uint32_t const ADC_STEPCONFIG_WRITE_PROTECT_OFF{0x01 << 2};
+    uint32_t const ADC_STEPENABLE{ADC_TSC + 0x54};
+
+    map[(ADC_CTRL - MMAP_OFFSET) / 4] &= !0x01;
+    map[(ADC_CTRL - MMAP_OFFSET) / 4] |= ADC_STEPCONFIG_WRITE_PROTECT_OFF;
+
+    uint32_t const ADCSTEPCONFIG1{ADC_TSC + 0x64};
+    uint32_t const ADCSTEPDELAY1{ADC_TSC + 0x68};
+    uint32_t const ADCSTEPCONFIG2{ADC_TSC + 0x6C};
+    uint32_t const ADCSTEPDELAY2{ADC_TSC + 0x70};
+    uint32_t const ADCSTEPCONFIG3{ADC_TSC + 0x74};
+    uint32_t const ADCSTEPDELAY3{ADC_TSC + 0x78};
+    uint32_t const ADCSTEPCONFIG4{ADC_TSC + 0x7C};
+    uint32_t const ADCSTEPDELAY4{ADC_TSC + 0x80};
+    uint32_t const ADCSTEPCONFIG5{ADC_TSC + 0x84};
+    uint32_t const ADCSTEPDELAY5{ADC_TSC + 0x88};
+    uint32_t const ADCSTEPCONFIG6{ADC_TSC + 0x8C};
+    uint32_t const ADCSTEPDELAY6{ADC_TSC + 0x90};
+    uint32_t const ADCSTEPCONFIG7{ADC_TSC + 0x94};
+    uint32_t const ADCSTEPDELAY7{ADC_TSC + 0x98};
+    uint32_t const ADCSTEPCONFIG8{ADC_TSC + 0x9C};
+    uint32_t const ADCSTEPDELAY8{ADC_TSC + 0xA0};
+
+    uint32_t const ADC_AVG8{0b011 << 2};
+
+    uint32_t const ADC_SW_ONESHOT{0b00};
+
+    map[(ADCSTEPCONFIG1 - MMAP_OFFSET) / 4] = 0x00 << 19 | ADC_AVG8 | ADC_SW_ONESHOT;
+    map[(ADCSTEPDELAY1 - MMAP_OFFSET) / 4]  = 0 << 24;
+    map[(ADCSTEPCONFIG2 - MMAP_OFFSET) / 4] = 0x01 << 19 | ADC_AVG8 | ADC_SW_ONESHOT;
+    map[(ADCSTEPDELAY2 - MMAP_OFFSET) / 4]  = 0 << 24;
+    map[(ADCSTEPCONFIG3 - MMAP_OFFSET) / 4] = 0x02 << 19 | ADC_AVG8 | ADC_SW_ONESHOT;
+    map[(ADCSTEPDELAY3 - MMAP_OFFSET) / 4]  = 0 << 24;
+    map[(ADCSTEPCONFIG4 - MMAP_OFFSET) / 4] = 0x03 << 19 | ADC_AVG8 | ADC_SW_ONESHOT;
+    map[(ADCSTEPDELAY4 - MMAP_OFFSET) / 4]  = 0 << 24;
+    map[(ADCSTEPCONFIG5 - MMAP_OFFSET) / 4] = 0x04 << 19 | ADC_AVG8 | ADC_SW_ONESHOT;
+    map[(ADCSTEPDELAY5 - MMAP_OFFSET) / 4]  = 0 << 24;
+    map[(ADCSTEPCONFIG6 - MMAP_OFFSET) / 4] = 0x05 << 19 | ADC_AVG8 | ADC_SW_ONESHOT;
+    map[(ADCSTEPDELAY6 - MMAP_OFFSET) / 4]  = 0 << 24;
+    map[(ADCSTEPCONFIG7 - MMAP_OFFSET) / 4] = 0x06 << 19 | ADC_AVG8 | ADC_SW_ONESHOT;
+    map[(ADCSTEPDELAY7 - MMAP_OFFSET) / 4]  = 0 << 24;
+    map[(ADCSTEPCONFIG8 - MMAP_OFFSET) / 4] = 0x07 << 19 | ADC_AVG8 | ADC_SW_ONESHOT;
+    map[(ADCSTEPDELAY8 - MMAP_OFFSET) / 4]  = 0 << 24;
+
+    map[(ADC_CTRL - MMAP_OFFSET) / 4] |= 0x01;
+
+    cluon::OD4Session od4{CID};
+
+    uint32_t const FIFO0COUNT{ADC_TSC + 0xE4};
+    uint32_t const FIFO_COUNT_MASK{0b01111111};
+
+    uint32_t const ADC_FIFO0DATA{ADC_TSC + 0x100};
+    uint32_t const ADC_FIFO_MASK{0xFFF};
+
+    auto atFrequency{[&map, &FIFO0COUNT, &MMAP_OFFSET, &FIFO_COUNT_MASK, &ADC_FIFO0DATA, &ADC_FIFO_MASK, &ADC_STEPENABLE, &CHANNEL, &ID, &VERBOSE, &od4]() -> bool
+      {
+        int32_t output;
+        while (map[(FIFO0COUNT - MMAP_OFFSET) / 4] & FIFO_COUNT_MASK) {
+          output =  map[(ADC_FIFO0DATA - MMAP_OFFSET) / 4] & ADC_FIFO_MASK;
+        }
+
+        map[(ADC_STEPENABLE - MMAP_OFFSET) / 4] |= (0x01 << (CHANNEL + 1));
+
+        while(!(map[(FIFO0COUNT - MMAP_OFFSET) / 4] & FIFO_COUNT_MASK)) {
+        }
+
+        output =  map[(ADC_FIFO0DATA - MMAP_OFFSET) / 4] & ADC_FIFO_MASK;
+        float voltage = output * 1.8f / 4095.0f;
+
+        opendlv::proxy::VoltageReading voltageReading;
+        voltageReading.voltage(voltage);
+
+        od4.send(voltageReading);
+        if (VERBOSE) {
+          std::cout << "Voltage reading is " << voltageReading.voltage() << " V." << std::endl;
+
+          double distance = 5 * std::pow(voltageReading.voltage(), -0.4);
+          std::cout << "Distance calculated from voltage reading is " << distance << " cm." << std::endl;
+        }
+        return true;
+      }};
+
+    od4.timeTrigger(FREQ, atFrequency);
+  }
+  return retCode;
+}
---
+++ opendlv-device-ultrasonic-srf08/src/opendlv-device-ultrasonic-srf08.cpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2018 Ola Benderius
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <fstream>
+#include <vector>
+
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "cluon-complete.hpp"
+#include "opendlv-standard-message-set.hpp"
+
+int32_t main(int32_t argc, char **argv) {
+  int32_t retCode{0};
+  auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
+  if (0 == commandlineArguments.count("cid") || 0 == commandlineArguments.count("freq") || 0 == commandlineArguments.count("dev") || 0 == commandlineArguments.count("bus-address")) {
+    std::cerr << argv[0] << " interfaces to an SRF02 ultrasonic distance sensor using i2c." << std::endl;
+    std::cerr << "Usage:   " << argv[0] << " --dev=<I2C device node> --bus-address=<Sensor address on the i2c bus, in decimal format> --freq=<Parse frequency> --cid=<OpenDaVINCI session> --cid2=<OpenDaVINCI session> [--id=<ID if more than one sensor>] [--verbose]" << std::endl;
+    std::cerr << "Example: " << argv[0] << " --dev=/dev/i2c-0 --bus-address=112 --freq=10 --cid=111" << std::endl;
+    retCode = 1;
+  } else {
+    uint32_t const ID{(commandlineArguments["id"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
+    bool const VERBOSE{commandlineArguments.count("verbose") != 0};
+    uint16_t const CID = std::stoi(commandlineArguments["cid"]);
+    uint16_t const CID2 = std::stoi(commandlineArguments["cid2"]);
+
+    float const FREQ = std::stof(commandlineArguments["freq"]);
+
+    std::string const devNode = commandlineArguments["dev"];
+    int16_t deviceFile = open(devNode.c_str(), O_RDWR);
+    if (deviceFile < 0) {
+      std::cerr << "Failed to open the i2c bus." << std::endl;
+      return 1;
+    }
+
+    uint8_t const address = std::stoi(commandlineArguments["bus-address"]);
+
+    int8_t status = ioctl(deviceFile, I2C_SLAVE, address);
+    if (status < 0) {
+      std::cerr << "Could not acquire bus access for device " << devNode << "." << std::endl;
+      return 1;
+    }
+
+    uint8_t parseFirmwareBuffer[1];
+    parseFirmwareBuffer[0] = 0x00;
+    status = write(deviceFile, parseFirmwareBuffer, 1);
+    if (status != 1) {
+      std::cerr << "Could not write firmware request to device on " << devNode << "." << std::endl;
+      return 1;
+    }
+
+    uint8_t firmwareBuffer[1];
+    status = read(deviceFile, firmwareBuffer, 1);
+    if (status != 1) {
+      std::cerr << "Could not read firmware from device on " << devNode << "." << std::endl;
+      return 1;
+    }
+
+    std::cout << "Connected with the SRF08 device on " << devNode << ". Reported firmware version '" << static_cast<int32_t>(firmwareBuffer[0]) << "'." << std::endl;
+
+    cluon::OD4Session od4{CID};
+    cluon::OD4Session od4_obstacle{CID2};
+
+    uint8_t commandBuffer[2];
+    commandBuffer[0] = 0x00;
+    commandBuffer[1] = 0x51;
+
+    auto atFrequency{[&deviceFile, &commandBuffer, &ID, &VERBOSE, &od4, &od4_obstacle]() -> bool
+      {
+        uint8_t rangeHiReg{0x02};
+        uint8_t rangeLoReg{0x03};
+        uint8_t lightSensorReg{0x01};
+	    
+        uint8_t rangeHi;
+        uint8_t rangeLo;
+        uint8_t lightSensor;
+
+        uint8_t res = write(deviceFile, commandBuffer, 2);
+        if (res != 2) {
+          std::cerr << "Could not write ranging request." << std::endl;
+          return false;
+        }
+
+        std::this_thread::sleep_for(std::chrono::duration<double>(0.07));
+
+        res = write(deviceFile, &rangeHiReg, 1);
+        res += read(deviceFile, &rangeHi, 1);
+        res += write(deviceFile, &rangeLoReg, 1);
+        res += read(deviceFile, &rangeLo, 1);
+        res += write(deviceFile, &lightSensorReg, 1);
+        res += read(deviceFile, &lightSensor, 1);
+        if (res != 6) {
+          std::cerr << "Could not read data." << std::endl;
+          return false;
+        }
+
+        uint32_t rangeM = (rangeHi << 8) + rangeLo;
+        float distance = static_cast<float>(rangeM) / 100.0f;
+
+        opendlv::proxy::DistanceReading distanceReading;
+        distanceReading.distance(distance);
+
+        cluon::data::TimeStamp sampleTime;
+
+        od4.send(distanceReading);
+
+        if((double)distanceReading.distance() < 0.2){
+          od4_obstacle.send(distanceReading);
+        }
+
+        return true;
+      }};
+
+    od4.timeTrigger(FREQ, atFrequency);
+  }
+  return retCode;
+}
